02e446558dd4c741276b1d24fb12937b0dbd4b86
Dhruv-Sharma01 <121188299+Dhruv-Sharma01@users.noreply.github.com>
2025-02-23 22:49:11 +0530

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,6 +1,7 @@
 #include "functions.h"
 #include <cmath>
-
+#include "octree.h"
+using namespace octree;
 namespace functions {
     Vec3 intersections(const Vec3& p1, const Vec3& p2, float v1, float v2) {
         float t = (0 - v1) / (v2 - v1);
@@ -8,4 +9,20 @@ namespace functions {
                     p1.y + t * (p2.y - p1.y),
                     p1.z + t * (p2.z - p1.z));
     }
+    vector<vector<vec3>> triangulate(){
+    std::vector<std::vector<vec3>> triangles;
+    for(auto j : octree::cell_edge){
+        std::vector<vec3> vertices;
+        for(auto i : j.second){
+            vertices.push_back(i->vertex);
+            if(vertices.size() == 3){
+                triangles.push_back(vertices);
+                vertices.clear();
+            }
+        }
+    }
+        return triangles;
+    // Optionally, do something with 'triangles' (e.g., return them, store them, or process them further)
+}
+
 }
